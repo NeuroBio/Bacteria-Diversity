ed7421ccc3e3aa332e723ed46369b4b5dd945cdb
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-07-18 15:48:08 -0400

    set up for deliverable 2

--- charts.js
+++ charts.js
@@ -104,8 +104,22 @@ function buildCharts(sample) {
      title: 'Top 10 Bacteria Cultures Found',
      yaxis: { tickvals: yticks }
     };
-    
+
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot('bar', barData, barLayout);
+
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [
+
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot('bubble', bubbleData, bubbleLayout);
   });
 }
