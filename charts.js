c558f48206484ebb1e7154b5d68b3ac8c0d1741a
Cristina Robinson <NeuroBio@users.noreply.github.com>
2021-07-18 15:36:21 -0400

    deliverable 1

--- charts.js
+++ charts.js
@@ -58,30 +58,55 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
+    const samples = data.samples;
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
+    const sampleArr = samples.filter(s => s.id = sample);
 
     //  5. Create a variable that holds the first sample in the array.
+    const firstSample = sampleArr[0];
 
 
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    const ids = firstSample.otu_ids;
+    const labels = firstSample.otu_labels;
+    const values = firstSample.sample_values;
 
     // 7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
 
-    var yticks = ''
+    const topIds = ids
+      .sort((a, b) => a > b)
+      .slice(0, 10)
+      .map(val => 'OTU ' + val)
+      .reverse();
+    const topLabels = labels
+      .sort((a, b) => a > b)
+      .slice(0, 10)
+      .reverse();
+    const topValues = values
+      .sort((a, b) => a > b)
+      .slice(0, 10)
+      .map(val => parseInt(val))
+      .reverse();
+
+    var yticks = topIds;
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      x: topValues,
+      y: yticks,
+      type: 'bar',
+      orientation: 'h',
+      text: topLabels
+    }];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+     title: 'Top 10 Bacteria Cultures Found',
+     yaxis: { tickvals: yticks }
     };
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot('bar', barData, barLayout);
   });
 }
